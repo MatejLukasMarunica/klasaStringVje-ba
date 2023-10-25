65ce99cacf9990d44b3a319f863ff82a7f157ec9
MatejLukasMarunica <145869520+MatejLukasMarunica@users.noreply.github.com>
2023-10-25 09:16:46 +0200

    Add files via upload

---
+++ klasaStringVježba3/klasaStringVježba3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaStringVježba3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1;
+
+            Console.WriteLine("  Unesi neke rečenice  ");
+            s1 = Console.ReadLine();
+
+            string prvarec = s1.Substring(0, s1.IndexOf(" "));
+            string zadnjirec = s1.Substring(s1.LastIndexOf(" ", s1.Length - 1));
+
+            Console.WriteLine(prvarec + zadnjirec);
+
+            Console.ReadKey();
+        }
+    }
+}
