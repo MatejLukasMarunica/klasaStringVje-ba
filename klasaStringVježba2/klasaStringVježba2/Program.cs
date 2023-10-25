1a82aedb9a2ccce8efd50b6877fc0f955d9d4250
MatejLukasMarunica <145869520+MatejLukasMarunica@users.noreply.github.com>
2023-10-25 08:58:37 +0200

    Add files via upload

---
+++ klasaStringVježba2/klasaStringVježba2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaStringVježba2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+             s = Console.ReadLine();
+
+            string sVelika = s.ToUpper();
+            string sMala = s.ToLower();
+            string sPrvaTri = s.Substring(0, 3);
+            string sZadnjihPet = s.Substring(s.Length - 5, 5);
+            string s8_11 = s;
+
+           
+                Console.WriteLine(sVelika);
+
+
+                Console.WriteLine(sMala);
+
+
+                Console.WriteLine(sPrvaTri);
+
+
+                Console.WriteLine(sZadnjihPet);
+
+            if(s.Length > 11)
+            {
+                s8_11 = s.Substring(8, 3);
+                Console.WriteLine(s8_11);
+            }
+            else
+            {
+                Console.WriteLine(" Tekst je prekratak");
+            }
+                Console.ReadKey();
+            
+               
+            }
+
+            
+        }
+    }
+
