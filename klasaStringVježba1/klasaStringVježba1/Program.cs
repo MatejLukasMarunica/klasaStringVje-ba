cc9beae20ed02a63d6d8ea640d4b71405fb9b1aa
MatejLukasMarunica <145869520+MatejLukasMarunica@users.noreply.github.com>
2023-10-25 08:21:34 +0200

    Add files via upload

---
+++ klasaStringVježba1/klasaStringVježba1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaStringVježba1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1;
+
+            Console.WriteLine("  Unesi neki string bilo koje dužine ");
+            s1 = Console.ReadLine();
+
+            Console.WriteLine(s1.Replace(" ", "_"));
+            
+
+            Console.ReadKey();
+        }
+    }
+}
